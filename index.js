56c13e1eb487249d30e50e5fcb3f2bc0835a5996
DoriTran <tranquocdong308@gmail.com>
2022-10-26 20:03:04 +0700

    vercel

--- index.js
+++ index.js
@@ -9,42 +9,6 @@ let cors = require("cors");
 app.use(cors());
 app.options('*', cors())
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-allowCors(handler)
-
-/* CROS middleware */
-app.use(function(req, res, next) {
-  // Mọi domain
-  res.header("Access-Control-Allow-Origin", "*");
- 
-  // Domain nhất định
-  // res.header("Access-Control-Allow-Origin", "domain");
- 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 // Read body post
 app.use(
   express.urlencoded({
